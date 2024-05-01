bff8f84f8373be0e331ebacaac3d6d537bc7e0a1
lilika67 <l.kayitesi@alustudent.com>
2024-05-01 21:32:11 +0200

    [delivers docs] homework collab documentation

--- Desktop/SheCanCode-practices4/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./html-folder/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
--- Documents/Projects/alu-scripting-main/regular_expressions/README.md
+++
@@ -1,1 +0,0 @@
-this is readme consist of regular expression syntax
